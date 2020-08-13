71dce8038126cdba4fd43b50395f5af3288119ee
Lankovv <maciej.lankow@gmail.com>
2020-08-13 22:48:20 +0200

    Active fix

--- js/roller.js
+++ js/roller.js
@@ -1,9 +1,10 @@
-// const menuTrigger = document.querySelector('#topbar-options-trigger');
-// const menu = document.querySelector('.topbar-wrapper');
-// menuTrigger.addEventListener('click', handleClick);
-// const handleClick = () => {
-//     menu.classList.toggle('topbar-options-active');
-//  }
+const pickTemplate = "<div class='main-pick'>" + $(".main-pick").eq(0).html() + "</div>";
+
+window.onload = function() {
+    if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
+        document.body.addEventListener('touchstart', function() {}, false);
+    }
+}
 
 $('.trigger').click(function(e) {
     $(this).siblings('.main-pick-menu').toggleClass('menu-active');
@@ -13,3 +14,17 @@ $('#topbar-options-trigger').click(function(e) {
     $('.topbar-wrapper').toggleClass('topbar-wrapper-active');
     $('.topbar-options').toggleClass('topbar-options-active');
 });
+
+$('.main-add').click(function(e) {
+    if($('.main-pick').length<5){
+        $( ".main-pick" ).eq(-1).after(function() {
+            return pickTemplate;
+            
+          });
+    if($('.main-pick').length==5){
+         $('.main-add').toggleClass('main-add-inactive');
+    }
+    }else{}
+});
+
+
